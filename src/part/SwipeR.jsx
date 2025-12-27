ca44d8796013c73344dc0015cb74f296e6854db8
yo-dalo <kumaradarsh00572@gmail.com>
2025-12-27 12:39:01 +0530

    swiper yani poster ko connct kr diya h

--- src/part/SwipeR.jsx
+++ src/part/SwipeR.jsx
@@ -1,28 +1,42 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
-
-
-
 import { Navigation } from 'swiper/modules';
+import Yo from './utils/Yo';
+import { useState } from 'react';
+
+
+
 
 const SwipeR = () => {
+
+    const [resData, setResData] = useState([])
+    useEffect(() => {
+
+        Yo.get("http://localhost:3000/api/client/poster").then((res) => {
+            setResData(res.data.data)
+            console.log(res.data.data)
+        })
+
+
+
+    }, [])
+
+
     return (
         <div className="w-screen z-50 h-[40vw] bg-main">
-            <Swiper navigation={true}  modules={[Navigation]} className="w-full z-1 h-full">
+            <Swiper navigation={true} modules={[Navigation]} className="w-full z-1 h-full">
 
                 {
-                    [
-                        "img/swiper/swiper1.jpeg",
-                        "img/swiper/swiper1.jpeg",
-                    ].map((e, i) => (
+
+                    resData.map((e, i) => (
 
 
                         <SwiperSlide key={i} className='FULL'>
-                            <img className='object-contain  ' src={e} alt="" />
+                            <img className='object-contain  ' src={`uploads/${e.Image}`} alt="" />
                         </SwiperSlide>
 
                     ))
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000', // Your backend URL
+        changeOrigin: true,
+     //rewrite: path => path.replace(/^\/api/, ''),
+      },
+      '/uploads': {
+        target: 'http://localhost:3000', // Your backend URL
+        changeOrigin: true,
+     //rewrite: path => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
