846e6bed628dd52b02e2cb7ba60446c6d5ec3d86
yo-dalo <kumaradarsh00572@gmail.com>
2025-12-29 23:37:47 +0530

    toper pge me kuch kiya h

--- src/part/home/PageEleven.jsx
+++ src/part/home/PageEleven.jsx
@@ -5,8 +5,8 @@ const PageEleven = () => {
     <div className="w-screen   overflow-hidden h-fit  center  ">
 
 
-      <div className='w-screen BT md:gap-10 md:FC md:h-fit phone:h-screen phone:FC bg-main  h-[70vh]'>
-        <div className="w-[45vw]  md:w-full phone:w-full  phone:gap-4  pl-3 pr-2 center gap-9 h-full ">
+      <div className='w-screen BT md:gap-10 md:FC md:h-fit phone:h-screen phone:FC bg-main  h-[450px]'>
+        <div className="w-[1162px] bg-slate-300  md:w-full phone:w-full  phone:gap-4  pl-3 pr-2 center gap-9 h-full ">
           <div className=" w-[20vw] md:w-[70vw] phone:w-[70vw] bg-mainOne h-[40vh] rounded-xl ">
 
           </div>
@@ -20,7 +20,7 @@ const PageEleven = () => {
             </div>
 
 
-            <h1 className='text-[25vw] phone:text-[40vw] phone:translate-x-0 md:-translate-x-[100px]  phone:translate-y-0 -translate-x-[5rem] translate-y-[1rem]  relative  phone:leading-[1] leading-[0.5]'> <span className='text-2xl right-[28px] -top-[25px]  md:right-[2px]  phone:right-[2px] phone:top-[15px] px-0 text-red-600 absolute'>st</span>1  </h1>
+            <h1 className='text-[25vw] phone:text-[40vw] phone:translate-x-0 md:-translate-x-[100px]  phone:translate-y-0 -translate-x-[120px] translate-y-[1rem]  relative  phone:leading-[1] leading-[0.5]'> <span className='text-2xl right-[1px] -top-[25px]  md:right-[2px]  phone:right-[2px] phone:top-[15px] px-0 text-red-600 absolute'>st</span>1  </h1>
 
             <h2 className='text-2xl  leading-6  phone:w-full  w-[58%]'>Position in Univarsty </h2>
 
