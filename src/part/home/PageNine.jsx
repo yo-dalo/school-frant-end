9bf8994fb8d83b1c5bacad82ae822a377e564c2b
yo-dalo <kumaradarsh00572@gmail.com>
2025-12-29 22:03:54 +0530

    responsiv kiya h thoda sa

--- src/part/home/PageNine.jsx
+++ src/part/home/PageNine.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion"
 
 const PageNine = () => {
   return (
-    <div className="w-full phone:h-[70vh] overflow-hidden h-[50vw] py-12 gap-5 grid grid-rows-2 bg-main">
+    <div className="w-full phone:h-[70vh] overflow-hidden h-[700px] py-12 gap-5 grid grid-rows-2 bg-main">
       
       {/* Row 1 : left */}
       <div className="overflow-hidden">
@@ -19,9 +19,9 @@ const PageNine = () => {
         >
           {[...Array(2)].map((_, i) => (
             <React.Fragment key={i}>
-              <img className="h-full" src="https://i.pinimg.com/736x/a0/d8/82/a0d882b093ecc4da0eecd34258b63423.jpg" />
-              <img className="h-full" src="https://i.pinimg.com/736x/2e/bb/49/2ebb49c2e9cee470cfbf140ff55908db.jpg" />
-              <img className="h-full" src="https://i.pinimg.com/736x/f3/f1/45/f3f145d5aa2f40f8c620900c60139c07.jpg" />
+              <img className="h-full object-fill" src="https://i.pinimg.com/736x/a0/d8/82/a0d882b093ecc4da0eecd34258b63423.jpg" />
+              <img className="h-full object-fill" src="https://i.pinimg.com/736x/2e/bb/49/2ebb49c2e9cee470cfbf140ff55908db.jpg" />
+              <img className="h-full object-fill " src="https://i.pinimg.com/736x/f3/f1/45/f3f145d5aa2f40f8c620900c60139c07.jpg" />
             </React.Fragment>
           ))}
         </motion.div>
@@ -41,9 +41,9 @@ const PageNine = () => {
         >
           {[...Array(2)].map((_, i) => (
             <React.Fragment key={i}>
-              <img className="h-full" src="https://i.pinimg.com/736x/2e/bb/49/2ebb49c2e9cee470cfbf140ff55908db.jpg" />
-              <img className="h-full" src="https://i.pinimg.com/736x/a0/d8/82/a0d882b093ecc4da0eecd34258b63423.jpg" />
-              <img className="h-full" src="https://i.pinimg.com/736x/f3/f1/45/f3f145d5aa2f40f8c620900c60139c07.jpg" />
+              <img className="h-full object-fill" src="https://i.pinimg.com/736x/2e/bb/49/2ebb49c2e9cee470cfbf140ff55908db.jpg" />
+              <img className="h-full object-fill" src="https://i.pinimg.com/736x/a0/d8/82/a0d882b093ecc4da0eecd34258b63423.jpg" />
+              <img className="h-full object-fill" src="https://i.pinimg.com/736x/f3/f1/45/f3f145d5aa2f40f8c620900c60139c07.jpg" />
             </React.Fragment>
           ))}
         </motion.div>
