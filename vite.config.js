b56bf09d6e0e67377e3a83dfe1f4aea915f26236
yo-dalo <kumaradarsh00572@gmail.com>
2026-03-04 01:14:00 +0530

    refactor: update API endpoint in SwipeR component and clean up Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   server: {
+  server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000', // Your backend URL
+        target: 'http://localhost:3000',
         changeOrigin: true,
-     //rewrite: path => path.replace(/^\/api/, ''),
+        configure: (proxy) => {
+          proxy.on('proxyReq', (proxyReq, req) => {
+            const host = req.headers.host;
+            proxyReq.setHeader('host', host);
+          });
+        },
       },
       '/uploads': {
-        target: 'http://localhost:3000', // Your backend URL
+        target: 'http://localhost:3000',
         changeOrigin: true,
-     //rewrite: path => path.replace(/^\/api/, ''),
+        configure: (proxy) => {
+          proxy.on('proxyReq', (proxyReq, req) => {
+            const host = req.headers.host;
+            proxyReq.setHeader('host', host);
+          });
+        },
       },
     },
   },
